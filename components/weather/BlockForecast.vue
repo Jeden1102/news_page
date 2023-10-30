00f54ac5e465dc800c654309b17ec79c0413d018
Jeden1102 <bum123@o2.pl>
2023-10-30 22:26:07 +0100

    master. Forecast block start.

---
+++ components/weather/BlockForecast.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ forecast }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+const props = defineProps<{
+  forecast: string;
+}>();
+</script>
+
+<style scoped>
+</style>
--- components/weather/BlockMain.vue
+++ components/weather/BlockMain.vue
@@ -4,26 +4,47 @@
     class="flex flex-col weather__main p-4 rounded-md text-black gap-6 md:p-6 bg-white mb-6 shadow-lg w-full lg:mb-0"
   >
     <WeatherBlockGeneral :weather="weather.list[0]" :town="town" :full="true" />
-    <div
-      v-for="(day, key) in groupForecast"
-      class="bg-gray-400 p-3 rounded-lg flex flex-col items-center justify-center w-full"
-    >
-      <p class="font-bold">{{ mapDay[key] }}</p>
-      <div class="flex justify-between w-full">
-        <span>{{ dayRangeTemp(day).min }}</span>
-        <span>{{ dayRangeTemp(day).max }}</span>
-      </div>
+    <WeatherBlockForecast :forecast="activeDay" />
+    <div class="w-full">
+      <swiper :space-between="12" :slidesPerView="'auto'">
+        <swiper-slide
+          class="bg-gray-200 border border-gray-300 shadow-md p-3 w-24 rounded-lg"
+          v-for="(day, key) in groupForecast"
+        >
+          <div
+            class="flex flex-col items-center justify-center cursor-pointer"
+            @click="activeDay = day"
+          >
+            <p class="font-bold">{{ mapDay[key] }}</p>
+            <img
+              class="w-fit"
+              :src="
+                'https://openweathermap.org/img/wn/' +
+                dayMiddleIcon(day).weather[0].icon +
+                '.png'
+              "
+            />
+
+            <div class="flex justify-center gap-4 w-full">
+              <span>{{ dayRangeTemp(day).min }}&deg;</span>
+              <span class="font-bold">{{ dayRangeTemp(day).max }}&deg;</span>
+            </div>
+          </div>
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
 import { onMounted, defineProps, ref, computed } from "vue";
 import { useWeatherStore } from "../../store/weather";
 const weatherStore = useWeatherStore();
 const props = defineProps<{
   town: string;
 }>();
+const activeDay = ref(null);
 const weather = ref(null);
 const mapDay = {
   "0": "Sun",
@@ -37,6 +58,11 @@ const mapDay = {
 onMounted(async () => {
   const data = await weatherStore.getWeather(props.town, false);
   weather.value = data;
+  const firstDayKey = Object.keys(groupForecast.value)[0];
+  activeDay.value = groupForecast.value[firstDayKey];
+});
+const dayMiddleIcon = computed(() => (day: string[]) => {
+  return day[(day.length / 2) | 0];
 });
 const dayRangeTemp = computed(() => (day) => {
   let minTemp = Infinity;
@@ -55,7 +81,7 @@ const dayRangeTemp = computed(() => (day) => {
     }
   });
 
-  return { min: minTemp, max: maxTemp };
+  return { min: minTemp.toFixed(), max: maxTemp.toFixed() };
 });
 const groupForecast = computed(() => {
   if (!weather.value) return;
@@ -71,14 +97,20 @@ const groupForecast = computed(() => {
   });
   return grouped;
 });
+
 watch(
   async () => props.town,
   async (first) => {
     const data = await weatherStore.getWeather(props.town, false);
     weather.value = data;
-    console.log(weather.value);
   }
 );
 </script>
 
-<style scoped lang="scss"></style>
+<style  lang="scss">
+.weather__main {
+  .swiper-slide {
+    width: fit-content !important;
+  }
+}
+</style>
